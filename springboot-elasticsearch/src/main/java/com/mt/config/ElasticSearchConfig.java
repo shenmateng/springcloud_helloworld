cacfee9a16307fab0402e4e542c2ecafe9c396ef
mateng <mateng962396@dingtalk.com>
2021-11-26 11:42:06 +0800

    [springcloud_helloworld] 增加es项目

--- springboot-elasticsearch/src/main/java/com/mt/config/ElasticSearchConfig.java
+++ springboot-elasticsearch/src/main/java/com/mt/config/ElasticSearchConfig.java
@@ -21,11 +21,10 @@ public class ElasticSearchConfig {
     // 注册 rest高级客户端
     @Bean
     public RestHighLevelClient restHighLevelClient(){
-        RestHighLevelClient client = new RestHighLevelClient(
+        return new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost("8.142.22.143",9200,"http")
                 )
         );
-        return client;
     }
 }
--- springboot-elasticsearch/src/test/java/com/mt/SpringbootElasticsearchApplicationTests.java
+++ springboot-elasticsearch/src/test/java/com/mt/SpringbootElasticsearchApplicationTests.java
@@ -1,13 +1,38 @@
 package com.mt;
 
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+
+
+import javax.annotation.Resource;
+import java.io.IOException;
 
 @SpringBootTest
 class SpringbootElasticsearchApplicationTests {
+
+    @Autowired
+    public RestHighLevelClient restHighLevelClient;
+
+    @Resource
+    private ElasticsearchRestTemplate elasticsearchRestTemplate;
 
     @Test
     void contextLoads() {
     }
 
+    // 测试索引的创建， Request PUT liuyou_index
+    @Test
+    public void testCreateIndex() throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest("mateng_index2");
+        CreateIndexResponse createIndexResponse = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+        System.out.println(createIndexResponse.isAcknowledged());// 查看是否创建成功
+        System.out.println(createIndexResponse);// 查看返回对象
+        restHighLevelClient.close();
+    }
 }
