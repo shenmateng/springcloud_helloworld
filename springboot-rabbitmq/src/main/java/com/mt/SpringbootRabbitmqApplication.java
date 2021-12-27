127f01eab63130780df3cd2b6d8e441cf4e15549
mateng <mateng962396@dingtalk.com>
2021-12-27 15:14:26 +0800

    rabbitmq项目添加

--- springboot-rabbitmq/src/main/java/com/mt/SpringbootRabbitmqApplication.java
+++ springboot-rabbitmq/src/main/java/com/mt/SpringbootRabbitmqApplication.java
@@ -3,6 +3,9 @@ package com.mt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author DELL
+ */
 @SpringBootApplication
 public class SpringbootRabbitmqApplication {
 
