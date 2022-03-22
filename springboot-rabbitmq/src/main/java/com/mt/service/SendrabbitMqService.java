4b85cb2c9ce994ea3fae6d20c2b1a3518fa4c11e
shenmateng <mt962396@163.com>
2022-03-22 21:26:39 +0800

    死信队列

--- springboot-rabbitmq/src/main/java/com/mt/service/SendrabbitMqService.java
+++ springboot-rabbitmq/src/main/java/com/mt/service/SendrabbitMqService.java
@@ -1,9 +1,24 @@
 package com.mt.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.mt.database.TemplateVevorListAndTaskDO;
 import com.mt.database.VevorNewListingReqVO;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Service;
+
+
+import com.rabbitmq.client.Channel;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -23,6 +38,11 @@ public class SendrabbitMqService {
 
     @Resource
     private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+
 
     public void testRabbitMq(){
 
@@ -47,5 +67,45 @@ public class SendrabbitMqService {
         templateVevorListAndTaskDO.setUsername(username);
         rabbitTemplate.convertAndSend(exchange, keys, templateVevorListAndTaskDO);
 
+    }
+
+    public void send(String queueName) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("email", "xx@163.com");
+        jsonObject.put("timestamp", 0);
+        String jsonString = jsonObject.toJSONString();
+        System.out.println("jsonString:" + jsonString);
+        // 设置消息唯一id 保证每次重试消息id唯一
+        Message message = MessageBuilder.withBody(jsonString.getBytes())
+                .setContentType(MessageProperties.CONTENT_TYPE_JSON).setContentEncoding("utf-8")
+                .setMessageId(UUID.randomUUID() + "").build(); //消息id设置在请求头里面 用UUID做全局ID
+        amqpTemplate.convertAndSend(queueName, message);
+    }
+
+
+
+    @RabbitListener(queues = "fanout_email_queue")
+    public void process(Message message, @Headers Map<String, Object> headers, Channel channel) throws Exception {
+        // 获取消息Id
+        String messageId = message.getMessageProperties().getMessageId();
+        String msg = new String(message.getBody(), "UTF-8");
+        System.out.println("邮件消费者获取生产者消息msg:" + msg + ",消息id" + messageId);
+
+        JSONObject jsonObject = JSONObject.parseObject(msg);
+        Integer timestamp = jsonObject.getInteger("timestamp");
+
+        try {
+            int result = 1 / timestamp;
+            System.out.println("result" + result);
+            // // 手动ack
+            Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+            // 手动签收
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception e) {
+            //拒绝消费消息（丢失消息） 给死信队列
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
+        }
+
+        System.out.println("执行结束....");
     }
 }
