78439be2dd8bc668fae956bcdae8795c6a2426db
mateng <962396475@qq.com>
2022-12-24 14:29:31 +0800

    增加es-parent父子依赖

---
+++ springboot-es-parent/springboot-es/src/main/com/mt/SpringbootEsApplication.java
@@ -0,0 +1,23 @@
+package mt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableAsync
+@EnableScheduling
+@EnableCaching
+public class SpringbootEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEsApplication.class, args);
+    }
+
+}
